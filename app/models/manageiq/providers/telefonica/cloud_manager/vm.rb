3c68df0e7c25ea1889360f5c8d0620b4bfde0839
pratik.awchat <pratikawchat@gmail.com>
2018-09-24 15:28:16 +0530

    Changes made for only destroy method

--- app/models/manageiq/providers/telefonica/cloud_manager/vm.rb
+++ app/models/manageiq/providers/telefonica/cloud_manager/vm.rb
@@ -30,9 +30,6 @@ class ManageIQ::Providers::Telefonica::CloudManager::Vm < ManageIQ::Providers::C
     "REBUILD"           => "wait_for_launch",
     "DELETED"           => "terminated",
     "MIGRATING"         => "migrating",
-    "LOCKED"            => "locked",
-    "UNLOCKED"          => "unlocked",
-
   }.freeze
 
   alias_method :private_networks, :cloud_networks
