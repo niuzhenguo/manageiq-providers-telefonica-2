7ec1f585590483549029c24ee883fc46e011138f
pratik.awchat <pratikawchat@gmail.com>
2018-09-24 15:27:41 +0530

    Changes made for only destroy method

--- app/models/manageiq/providers/telefonica/cloud_manager/vm/operations/guest.rb
+++ app/models/manageiq/providers/telefonica/cloud_manager/vm/operations/guest.rb
@@ -19,22 +19,6 @@ module ManageIQ::Providers::Telefonica::CloudManager::Vm::Operations::Guest
             raw_terminate
           end
       end
-
-    supports :lock_guest do
-        unsupported_reason_add(:lock_guest, unsupported_reason(:control)) unless supports_control?
-        unsupported_reason_add(:lock_guest, _("The VM is not in on, off or unlocked")) unless (current_state == "on") || (current_state == "off")
-        if (current_state == "on") || (current_state == "off")
-          raw_lock_guest
-        end
-    end
-
-    supports :unlock_guest do
-      unsupported_reason_add(:unlock_guest, unsupported_reason(:control)) unless supports_control?
-      unsupported_reason_add(:unlock_guest, _("The VM is not locked")) unless (current_state == "on") || (current_state == "off")
-      if (current_state == "on") || (current_state == "off")
-        raw_unlock_guest
-      end
-    end
 end
 
   def raw_reboot_guest
@@ -47,16 +31,6 @@ end
     with_provider_object { |instance| instance.reboot("HARD") }
     # Temporarily update state for quick UI response until refresh comes along
     self.update_attributes!(:raw_power_state => "HARD_REBOOT")
-  end
-
-  def raw_lock_guest
-    with_provider_object(&:lock)
-    # Temporarily update state for quick UI response until refresh comes along
-  end
-
-  def raw_unlock_guest
-    with_provider_object(&:unlock)
-    # Temporarily update state for quick UI response until refresh comes along
   end
 end
 
